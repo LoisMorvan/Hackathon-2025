0277c22ee566f56f7012124d0bb50ddc8dff523b
Elowan MESTRES <elowanmestres@gmail.com>
2025-05-06 12:14:14 +0200

    feat-accueil

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import index, communes, etablissements
 
 app = FastAPI(
     title="API Communes 44",
     description="API de recherche de communes en Loire-Atlantique 🐌",
     version="1.0"
+)
+
+# Configuration CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Autorise les requêtes depuis le frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Autorise toutes les méthodes HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Autorise tous les en-têtes
 )
 
 # Inclusion des routes
